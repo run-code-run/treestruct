6d625cf98b4dc0abdcabfedcb6e19c181f780da7
alex <alex7ey1@gmail.com>
2018-11-03 00:23:45 +0300

    initial

---
+++ src/main/java/directorybrowser/demo/IndexController.java
@@ -0,0 +1,13 @@
+package directorybrowser.demo;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index() {
+        return "index";
+    }
+
+}
